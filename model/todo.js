8e1135322539561fe3c299c72420204ddfe0a685
emresorkun <emresorkun@gmail.com>
2021-02-02 07:43:53 +0100

    model folder created

---
+++ model/todo.js
@@ -0,0 +1,12 @@
+const mongoose= require("mongoose");
+
+
+const todoSchema= new mongoose.Schema({
+
+    name: {type:String, required},
+    date: {type: Date, default: date.now}
+})
+
+const Todo= mongoose.model("todo", todoSchema);
+
+module.exports=Todo;
