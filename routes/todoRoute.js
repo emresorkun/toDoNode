6d25b39f241528f3dc7b51523c817e6a68c8b620
emresorkun <emresorkun@gmail.com>
2021-02-08 18:30:21 +0100

    sort and pagination files fixed and I also tried to serve the edit.ejs using the same css file. failed m

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,110 +1,80 @@
-const express= require("express");
-const Todo=require("../model/todo")
-const router= express.Router();
+const express = require("express");
+const Todo = require("../model/todo");
+const router = express.Router();
 
+router.get("/", async (req, res) => {
+  //query string
 
+  const sorted = +req.query.sorted || 1;
+  const page = +req.query.page || 1;
 
-
-
-
-
-
-
-
-
-
-
-router.get("/", async (req, res)=>{
-    //query string
-
-    const sorted= +req.query.page;
-    const page= +req.query.page;
-
-
-try{
-
-
-    const totalData= await Todo.find().count();
+  try {
+    const totalData = await Todo.find().countDocuments();
 
     const dataToShowPerReq = 2;
 
-    const totalDataPart = totalData/dataToShowPerReq;
+    const totalDataPart = Math.ceil(totalData/dataToShowPerReq);
 
+    //const totalDataPart = totalData / dataToShowPerReq;
 
-    const dataToShow = dataToShowPerReq * page
+    const dataToShow = dataToShowPerReq * page;
 
+    const data = await Todo.find()
+      .sort({ name: sorted })
+      .limit(dataToShow);
 
-    const data = await Todo.find().sort({name:sorted}).limit(dataToShowPerReq)
-    
-    res.render("index.ejs", 
-    {data,
-    totalData,
-    totalDataPart,
-    dataToShow,
-    dataToShowPerReq,
-    error:"empty"})
-}
-
-
-
-
-
-catch(err){ 
-    res.render("error.ejs", {error: err})
-    }
-
-
-
-})
+    res.render("index.ejs", {
+      data,
+      totalData,
+      totalDataPart,
+      dataToShow,
+      dataToShowPerReq,
+      error: "empty",
+    });
+  } catch (err) {
+    res.render("error.ejs", { error: err });
+  }
+});
 
 //path.join(__dirname, scss)
 
+router.post("/", async (req, res) => {
+  console.log(req.body.name);
 
-
-router.post("/", async (req, res)=>{
-
-    console.log (req.body.name)
- 
-    try{
+  try {
     await new Todo({
-        name:req.body.name
+      name: req.body.name,
     }).save();
-    res.redirect("/")
+    res.redirect("/");
+  } catch (err) {
+    res.render("error.ejs", { error: err });
+  }
+});
+
+router.get("/edit/:id", async (req, res) => {
+  console.log(req.query);
+  const todo = await Todo.findOne({ _id: req.params.id });
+  console.log(todo);
+
+  res.render("edit.ejs", { todo: todo });
+  //console.log("editing content")
+});
+
+router.post("/edit", async (req, res) => {
+  console.log(req.body);
+  await Todo.updateOne(
+    { _id: req.body.id },
+    {
+      name: req.body.name,
     }
-    catch(err){
+  );
 
-        res.render("error.ejs", {error: err})
-    }
+  res.redirect("/");
+});
 
-})
+router.get("/delete/:id", async (req, res) => {
+  await Todo.deleteOne({ _id: req.params.id });
+  res.redirect("/");
+});
 
-router.get("/edit/:id", async (req, res)=>{
-     console.log(req.query)
-    const todo=await Todo.findOne({_id:req.params.id})
-    console.log(todo)
-
-    res.render("edit.ejs", {todo:todo})
-    //console.log("editing content")
-
-} )
-
-router.post("/edit", async (req,res)=>{
-
-    console.log(req.body)
-    await Todo.updateOne( {_id:req.body.id}, {
-        name:req.body.name
-    })
-
-
-    res.redirect("/")
-})
-
-router.get("/delete/:id", async (req, res)=>{
-
-    await Todo.deleteOne({_id:req.params.id})
-    res.redirect("/")
-
-} )
-
-
-module.exports=router;
+module.exports = router;
